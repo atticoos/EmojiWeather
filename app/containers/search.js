bf726351e791bd93b3d959fea15d8d55ba7cf681
Atticus White <contact@atticuswhite.com>
2016-02-06 15:47:23 -0500

    sarch navbar

--- app/containers/search.js
+++ app/containers/search.js
@@ -3,17 +3,38 @@
 import React, {
   Component,
   StyleSheet,
+  TouchableOpacity,
+  TextInput,
   View,
   Text
 } from 'react-native';
 import Emoji from 'react-native-emoji';
 import Colors from '../constants/colors';
 import Styles from '../constants/styles';
+import NavBar from '../components/navBar';
 
 class Search extends Component {
+  getLeftNavItem() {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigator.pop()}>
+        <Text style={styles.done}>Done</Text>
+      </TouchableOpacity>
+    );
+  }
+  getSearchBar() {
+    return (
+      <View style={styles.searchWrapper}>
+        <TextInput style={styles.searchBar}></TextInput>
+      </View>
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
+        <NavBar
+          leftItem={this.getLeftNavItem()}
+          centerItem={this.getSearchBar()} />
         <Text style={{fontSize: 64}}>Search</Text>
       </View>
     );
@@ -25,6 +46,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.Blue,
     padding: Styles.screenPadding
+  },
+  done: {
+    color: '#fff',
+    fontSize: 18
+  },
+  searchWrapper: {
+    flex: 1,
+    height: 30,
+    backgroundColor: 'blue'
+  },
+  searchBar: {
+    flex: 1,
+    height: 30,
+    backgroundColor: 'blue'
   }
 });
 
